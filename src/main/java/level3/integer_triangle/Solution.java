102c81b0925846d96b320225d0b6cbcc537ab2ef
Han Hoon <gksgns456@kaoni.com>
2021-05-13 14:59:11 +0900

    init test

--- src/main/java/level3/integer_triangle/Solution.java
+++ src/main/java/level3/integer_triangle/Solution.java
@@ -35,12 +35,10 @@ class Solution {
 }
 
 class SolutionRunner {
-
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
         System.out.println(sol.solution(triangle));
     }
-
 }
 
